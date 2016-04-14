6f816fa7b1d03220c5489af349fb2a03fdd04361
hamsterofdark <mlandryviolin@gmail.com>
2016-04-13 23:13:26 -0400

    stuff

---
+++ src/OdeToFood/Services/Greeter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Services
+{
+    public interface IGreeter
+    {
+        String getGreeting();
+
+    }
+
+
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+       {
+            _greeting = configuration["greeting"];
+
+       }
+        public string getGreeting()
+        {
+            return _greeting;
+        }
+
+
+       
+    }
+
+}
--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -6,25 +6,66 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Services;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSingleton(provider => Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(
+            IApplicationBuilder app, 
+            IGreeter greeter,
+            IHostingEnvironment environment)
         {
             app.UseIISPlatformHandler();
 
+            if (environment.IsDevelopment())  //Logical environment information inferred from an enviroment variable ("Hosting:Envirconment - Development")
+            {
+                app.UseDeveloperExceptionPage();
+
+            }
+
+
+
+
+            app.UseRuntimeInfoPage();
+
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles();
+
+            app.UseFileServer();
+
+            app.UseMvcWithDefaultRoute();
+
+
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //throw new System.Exception("Error");
+
+
+                var greeting = greeter.getGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
 
